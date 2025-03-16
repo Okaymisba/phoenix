3c1c0cb46d7e3fad496cc0d430c614e18d821b26
okaymisba <msbahsarfaraz@gmail.com>
2025-03-16 17:56:43 +0500

    Refactor: Implement user authentication and authorization
    This commit introduces a comprehensive authentication and authorization system using bcrypt for password hashing, Ecto for database interactions, and user tokens for session management.
    
    Key changes include:
    - Added `Helloworld.Accounts` context for user management.
    - Implemented user registration, login, logout, password reset, and email confirmation workflows.
    - Introduced `HelloworldWeb.UserAuth` module for authentication logic in controllers and LiveViews.
    - Created controllers for user registration, session management, password reset, email confirmation, and settings.
    - Updated routes to include authentication-related paths.
    - Added database migrations for users and user tokens tables.
    - Modified the test environment to reduce the bcrypt hashing rounds.
    - Removed the default Phoenix Framework links from the application layout.
    - Added a Google icon to use with Social login buttons.
    
    This commit sets the foundation for a secure and robust user authentication system.

--- lib/helloworld_web/controllers/page_controller.ex
+++ lib/helloworld_web/controllers/page_controller.ex
@@ -4,6 +4,10 @@ defmodule HelloworldWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    if conn.assigns[:current_user] do
+      render(conn, :home, layout: false)
+    else
+      redirect(conn, to: ~p"/users/log_in")
+    end
   end
 end
